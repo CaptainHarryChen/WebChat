4fe763588d1b480daef97ea6eb09b4cd313ed394
CaptainHarryChen <captainharrychen@qq.com>
2022-04-18 23:56:07 +0800

    完成登录功能，用户密码数据库读取

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from flask import Flask
+import os
+import sqlite3
+from flask import Flask, request, render_template, session, redirect
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return app.send_static_file("index.html")
+    return render_template("index.html", login_state="1")
 
 
-if __name__=="__main__":
+@app.route("/login", methods=("POST",))
+def login():
+    user_name = request.form["user-name"]
+    password = request.form["password"]
+
+    usersDB = sqlite3.connect("users.db")
+    cur = usersDB.execute("select password from users where name='"+user_name+"'")
+    DBpwd = cur.fetchone()
+    if DBpwd is None:
+        return render_template("index.html", login_state="user-not-exist")
+    if password != DBpwd[0]:
+        return render_template("index.html", login_state="password-error")
+    session["userName"]=user_name
+    return redirect("/chat")
+
+
+@app.route("/chat")
+def chat():
+    return app.send_static_file("chat.html")
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run(host="0.0.0.0", port=80, debug=True)
