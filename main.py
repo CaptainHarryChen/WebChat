5ab30eae00af31f9be4f8d8ec050641f1dc5a449
Captain Chen <2429643516@qq.com>
2022-04-19 11:08:46 +0800

    完成注册功能

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from flask import Flask, request, render_template, session, redirect
 app = Flask(__name__)
 
 
+@app.route("/CheckUserName", methods=("POST",))
+def CheckUserName():
+    user_name = request.form["user_name"]
+    usersDB = sqlite3.connect("users.db")
+    cur = usersDB.execute(
+        "select password from users where name='"+user_name+"'")
+    DBpwd = cur.fetchone()
+    if DBpwd is None:
+        return "1"
+    return "0"
+
+
 @app.route("/")
 def index():
     return render_template("index.html", login_state="1")
@@ -16,14 +28,40 @@ def login():
     password = request.form["password"]
 
     usersDB = sqlite3.connect("users.db")
-    cur = usersDB.execute("select password from users where name='"+user_name+"'")
+    cur = usersDB.execute(
+        "select password from users where name='"+user_name+"'")
     DBpwd = cur.fetchone()
     if DBpwd is None:
         return render_template("index.html", login_state="user-not-exist")
     if password != DBpwd[0]:
         return render_template("index.html", login_state="password-error")
-    session["userName"]=user_name
+    session["userName"] = user_name
     return redirect("/chat")
+
+
+@app.route("/regis", methods=("POST",))
+def regis():
+    user_name = request.form["user-input"]
+    pwd = request.form["pwd"]
+    pwd_rp = request.form["pwd_rp"]
+
+    usersDB = sqlite3.connect("users.db")
+    cur = usersDB.execute(
+        f"select password from users where name='{user_name}'")
+    DBpwd = cur.fetchone()
+    if DBpwd is not None:
+        return render_template("register.html", regis_state="user-exist")
+    if pwd != pwd_rp:
+        return render_template("register.html", regis_state="password-error")
+    usersDB.execute(
+        f"insert into users (name,password) values('{user_name}','{pwd}')")
+    usersDB.commit()
+    return app.send_static_file("register_success.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html", regis_state="")
 
 
 @app.route("/chat")
